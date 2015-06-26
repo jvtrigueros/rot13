f99970829213108a7f70cb838307e80f81a2ebd5
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-06-26 00:02:51 -0600

    Add is-uppercase test for ranges.

--- test/rot13/algorithm_test.clj
+++ test/rot13/algorithm_test.clj
@@ -8,3 +8,7 @@
 (deftest lowercase-test
   (testing "All lowercase letters should return true."
     (is (= true (every? rot13.algorithm/is-lowercase? (seq lower-alpha))))))
+
+(deftest uppercase-test
+  (testing "All uppercase letters should return true."
+    (is (= true (every? rot13.algorithm/is-uppercase? (seq upper-alpha))))))
